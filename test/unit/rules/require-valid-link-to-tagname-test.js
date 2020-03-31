42f19e8d7a4fbaa8216c100cf7a260d717e28601
Joseph D. Sumner <me@jds.dev>
2020-03-31 13:50:56 -0500

    Add support for BlockStatement format

--- test/unit/rules/require-valid-link-to-tagname-test.js
+++ test/unit/rules/require-valid-link-to-tagname-test.js
@@ -9,23 +9,31 @@ generateRuleTests({
 
   config: true,
 
-  // TODO update with a good example that should pass
   good: [
-    // '{{#link-to tagName="button"}}foo{{/link-to}}',
-    '<LinkTo>foo</LinkTo>',
-    '<LinkTo tagName="a">foo</LinkTo>',
-
+    '<LinkTo @route="routeName">foo</LinkTo>',
+    '<LinkTo @route="routeName" tagName="a">foo</LinkTo>',
+    '{{#link-to "routeName"}}foo{{/link-to}}',
+    '{{#link-to "routeName" tagName="a"}}foo{{/link-to}}',    
 ],
 
   bad: [
     {
-      template: '<LinkTo tagName="button">foo</LinkTo>',
+      template: '<LinkTo @route="routeName" tagName="button">foo</LinkTo>',
       result: {
-        source: '<LinkTo tagName="button">foo</LinkTo>',
+        source: '<LinkTo @route="routeName" tagName="button">foo</LinkTo>',
         line: 1,
         column: 0,
         message: ERROR_MESSAGE
       },
-    }
+    },
+    {
+      template: '{{#link-to "routeName" tagName="button"}}foo{{/link-to}}',
+      result: {
+        source: '{{#link-to "routeName" tagName="button"}}foo{{/link-to}}',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE
+      },
+    },
   ],
 });
