eb8b5957203a943e4a268fe13ce193a0234f1ae1
Joseph D. Sumner <me@jds.dev>
2020-04-13 11:09:42 -0500

    Rebase onto upstream/master

--- test/unit/rules/require-valid-link-to-tagname-test.js
+++ test/unit/rules/require-valid-link-to-tagname-test.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const generateRuleTests = require('../../helpers/rule-test-harness');
+<<<<<<< HEAD
+=======
+
+>>>>>>> Generate files + POC for ElementNodes
 const ERROR_MESSAGE = require('../../../lib/rules/require-valid-link-to-tagname').ERROR_MESSAGE;
 
 generateRuleTests({
@@ -8,15 +12,25 @@ generateRuleTests({
 
   config: true,
 
+<<<<<<< HEAD
   good: [
     '<LinkTo @route="routeName">foo</LinkTo>',
     '<LinkTo @route="routeName" tagName="a">foo</LinkTo>',
     '{{#link-to "routeName"}}foo{{/link-to}}',
     '{{#link-to "routeName" tagName="a"}}foo{{/link-to}}',    
+=======
+  // TODO update with a good example that should pass
+  good: [
+    // '{{#link-to tagName="button"}}foo{{/link-to}}',
+    '<LinkTo>foo</LinkTo>',
+    '<LinkTo tagName="a">foo</LinkTo>',
+
+>>>>>>> Generate files + POC for ElementNodes
 ],
 
   bad: [
     {
+<<<<<<< HEAD
       template: '<LinkTo @route="routeName" tagName="button">foo</LinkTo>',
       result: {
         source: '<LinkTo @route="routeName" tagName="button">foo</LinkTo>',
@@ -29,10 +43,19 @@ generateRuleTests({
       template: '{{#link-to "routeName" tagName="button"}}foo{{/link-to}}',
       result: {
         source: '{{#link-to "routeName" tagName="button"}}foo{{/link-to}}',
+=======
+      template: '<LinkTo tagName="button">foo</LinkTo>',
+      result: {
+        source: '<LinkTo tagName="button">foo</LinkTo>',
+>>>>>>> Generate files + POC for ElementNodes
         line: 1,
         column: 0,
         message: ERROR_MESSAGE
       },
+<<<<<<< HEAD
     },
+=======
+    }
+>>>>>>> Generate files + POC for ElementNodes
   ],
 });
