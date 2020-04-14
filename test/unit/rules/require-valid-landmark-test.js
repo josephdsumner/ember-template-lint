bf1326667a574edcd76f921d4275974de9c6198e
Joseph D. Sumner <me@jds.dev>
2020-04-13 21:05:32 -0500

    Add failing tests + sort suite by serverity

--- test/unit/rules/require-valid-landmark-test.js
+++ test/unit/rules/require-valid-landmark-test.js
@@ -35,17 +35,183 @@ generateRuleTests({
     // Exceptions
     '<form role="search"></form>',
     '<footer role="contentinfo"></footer>',
-    
+
   ],
 
 
   bad: [
 
-    // Landmark elements with role assignments
+    /**
+    Landmark elements with role attribute assignments are sometimes
+    divided into two failure severity classes:
+
+    1. Conflicts -- never okay, role override causes severe issues: 
+      <nav role="main"></nav>
+      <nav role="arbitrary"></nav>
+    
+    2. Redundancies -- not best practice, but landmarks function:
+      <nav role="navigation"></nav>
+      <nav role=""></nav>
+
+    The current rule logic treats all of these as failures, but tests
+    are organized as above to simplify any future need to refactor 
+    to a severity configuration.
+    */
+
+    // 1. Conflicts
     {
       template: '<aside role="main"></aside>',
       result: {
         source: '<aside role="main"></aside>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    {
+      template: '<aside role="arbitrary"></aside>',
+      result: {
+        source: '<aside role="arbitrary"></aside>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+
+    {
+      template: '<footer role="main"></footer>',
+      result: {
+        source: '<footer role="main"></footer>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    {
+      template: '<footer role="arbitrary"></footer>',
+      result: {
+        source: '<footer role="arbitrary"></footer>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+
+    {
+      template: '<form role="main"></form>',
+      result: {
+        source: '<form role="main"></form>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    {
+      template: '<form role="arbitrary"></form>',
+      result: {
+        source: '<form role="arbitrary"></form>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+
+    {
+      template: '<header role="main"></header>',
+      result: {
+        source: '<header role="main"></header>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    {
+      template: '<header role="arbitrary"></header>',
+      result: {
+        source: '<header role="arbitrary"></header>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+
+    {
+      template: '<main role="navigation"></main>',
+      result: {
+        source: '<main role="navigation"></main>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    {
+      template: '<main role="arbitrary"></main>',
+      result: {
+        source: '<main role="arbitrary"></main>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+
+    {
+      template: '<nav role="main"></nav>',
+      result: {
+        source: '<nav role="main"></nav>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    {
+      template: '<nav role="arbitrary"></nav>',
+      result: {
+        source: '<nav role="arbitrary"></nav>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+
+    {
+      template: '<section role="main"></section>',
+      result: {
+        source: '<section role="main"></section>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    {
+      template: '<section role="arbitrary"></section>',
+      result: {
+        source: '<section role="arbitrary"></section>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+
+
+    // 2. Redundancies
+    {
+      template: '<aside role="complementary"></aside>',
+      result: {
+        source: '<aside role="complementary"></aside>',
         line: 1,
         column: 0,
         message: ERROR_MESSAGE,
@@ -61,11 +227,110 @@ generateRuleTests({
         message: ERROR_MESSAGE,
         moduleId: 'layout',
       },
+    },    
+    
+    {
+      template: '<header role="banner"></header>',
+      result: {
+        source: '<header role="banner"></header>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
     },
     {
-      template: '<aside role="some-role"></aside>',
+      template: '<header role=""></header>',
       result: {
-        source: '<aside role="some-role"></aside>',
+        source: '<header role=""></header>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },  
+
+    {
+      template: '<main role="main"></main>',
+      result: {
+        source: '<main role="main"></main>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    {
+      template: '<main role=""></main>',
+      result: {
+        source: '<main role=""></main>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },  
+
+    {
+      template: '<nav role="navigation"></nav>',
+      result: {
+        source: '<nav role="navigation"></nav>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    
+    {
+      template: '<nav role=""></nav>',
+      result: {
+        source: '<nav role=""></nav>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },  
+  
+    {
+      template: '<section role="region"></section>',
+      result: {
+        source: '<section role="region"></section>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },
+    
+    {
+      template: '<section role=""></section>',
+      result: {
+        source: '<section role=""></section>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },  
+
+
+    // Explicit redundancies for footer + form are exceptions
+    {
+      template: '<footer role=""></footer>',
+      result: {
+        source: '<footer role=""></footer>',
+        line: 1,
+        column: 0,
+        message: ERROR_MESSAGE,
+        moduleId: 'layout',
+      },
+    },  
+    {
+      template: '<form role=""></form>',
+      result: {
+        source: '<form role=""></form>',
         line: 1,
         column: 0,
         message: ERROR_MESSAGE,
