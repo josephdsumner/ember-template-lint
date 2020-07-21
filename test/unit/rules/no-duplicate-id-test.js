2c57995cb586b5536233c7aa0b51cabd6ad9e39b
Joseph D. Sumner <me@jds.dev>
2020-07-21 10:03:37 -0500

    Add support for Mustache with dynamic value

--- test/unit/rules/no-duplicate-id-test.js
+++ test/unit/rules/no-duplicate-id-test.js
@@ -13,13 +13,15 @@ generateRuleTests({
     // Unique sibling TextNode IDs
     '<div id="id-00"></div><div id="id-01"></div>',
 
-    // Ignore MustacheStatements, even duplicates
-    '<div id={{this.divId}}></div>',
-    '<div id={{this.divId}}></div><div id={{this.divId}}></div>',
+    // MustacheStatements
+    '<div id={{"id-00"}}></div>',
+    '<div id={{"id-00"}}></div><div id={{"id-01"}}></div>',
+    '<div id={{this.divId00}}></div>',
+    '<div id={{this.divId00}}></div><div id={{this.divId01}}></div>',
 
-    // Ignore ConcatStatements, even duplicates
-    '<div id="concat-{{this.divId}}"></div>',
-    '<div id="concat-{{this.divId}}"></div><div id="concat-{{this.divId}}"></div>',
+    // ConcatStatements
+    '<div id="concat-{{this.divId00}}"></div>',
+    '<div id="concat-{{this.divId00}}"></div><div id="concat-{{this.divId01}}"></div>',
 
     // Mustache and Concat do not conflict/flag with TextNode
     '<div id={{id-00}}></div><div id="id-00"></div>',
@@ -27,15 +29,19 @@ generateRuleTests({
     '<div id="concat-{{id-00}}"></div><div id="concat-id-00"></div>',
     '<div id="concat-id-00"></div><div id="concat-{{id-00}}"></div>',
 
-    // BlockStatement
+    // SourceNode: BlockStatement
     '<div id="id-00"></div>{{#foo elementId="id-01"}}{{/foo}}',
     '{{#foo elementId="id-01"}}{{/foo}}<div id="id-00"></div>',
 
-    // [WIP] Number + StringLiteral + Dynamic
+    // [WIP] Number
     '<div id={{1234}}></div>',
-    // '<div id={{"1234"}}></><div id={{"5678"}}></div>',
+    '<div id={{1234}}></div><div id={{"1234"}}></div>',
+
+    // [WIP] Dynamic
     '<div id={{"id-00"}}></div><div id={{"id-01"}}></div>',
     '<div id={{this.foo}}></div><div id={{this.bar}}></div>',
+
+    // Source: Mustache
     '{{foo id="id-00"}}{{foo id="id-01"}}',
   ],
 
@@ -170,28 +176,7 @@ generateRuleTests({
       },
     },
 
-    // // NumberLiteral
-    // {
-    //   template: '<div id="1234"></div><div id={{1234}}></div>',
-    //   result: {
-    //     message: ERROR_MESSAGE,
-    //     line: 1,
-    //     column: 26,
-    //     source: 'id={{1234}}',
-    //   },
-    // },
-
-    // {
-    //   template: '<div id={{1234}}></div><div id="1234"></div>',
-    //   result: {
-    //     message: ERROR_MESSAGE,
-    //     line: 1,
-    //     column: 28,
-    //     source: 'id="1234"',
-    //   },
-    // },
-
-    // Mustache
+    // Source Node: MustacheStatement
     {
       template: '{{foo id="id-00"}}{{foo id="id-00"}}',
       result: {
@@ -199,6 +184,28 @@ generateRuleTests({
         line: 1,
         column: 18,
         source: '{{foo id="id-00"}}',
+      },
+    },
+
+    // MustacheStatement with NumberLiteral
+    {
+      template: '<div id={{1234}}></div><div id={{1234}}></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 28,
+        source: 'id={{1234}}',
+      },
+    },
+
+    // MustacheStatement with Dynamic/Computed Value
+    {
+      template: '<div id={{this.divId00}}></div><div id={{this.divId00}}></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 36,
+        source: 'id={{this.divId00}}',
       },
     },
   ],
