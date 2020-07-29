a6367813b72942027bbf9c24d19a8405b5e82b23
Joseph D. Sumner <me@jds.dev>
2020-07-29 15:13:40 -0500

    parent 8559841b8a3b24be2a2e518a669ef18ce85a0e57 author Joseph D. Sumner <me@jds.dev> 1596053620 -0500 committer Joseph D. Sumner <me@jds.dev> 1597078894 -0500
    parent 8559841b8a3b24be2a2e518a669ef18ce85a0e57
    author Joseph D. Sumner <me@jds.dev> 1596053620 -0500
    committer Joseph D. Sumner <me@jds.dev> 1597078793 -0500
    
    parent 8559841b8a3b24be2a2e518a669ef18ce85a0e57
    author Joseph D. Sumner <me@jds.dev> 1596053620 -0500
    committer Joseph D. Sumner <me@jds.dev> 1597078621 -0500
    
    POC: testNode/refNode testNode/refNodesArray
    
    Add checks to helper functions: existence
    
    Chore: naming convention
    
    Update tests: add parser + make examples relevant
    
    Add no-match test for [refNodes]
    
    POC: generic `match(testNode, refNode OR [refNode])`
    
    Consolidate to generic `.match()` function + update tests
    
    Remove package-lock.json
    
    Resolve conflicts
    
    POC: direct import of `jscodeshift` `match` api
    
    Remove `recast` dependency (from POC )
    
    Chore: post-rebase-squash cleanup
    
    Chore: resolve conflicts
    
    POC: testNode/refNode testNode/refNodesArray
    
    Add checks to helper functions: existence
    
    Chore: naming convention
    
    Update tests: add parser + make examples relevant
    
    Add no-match test for [refNodes]
    
    POC: generic `match(testNode, refNode OR [refNode])`
    
    Consolidate to generic `.match()` function + update tests
    
    Remove package-lock.json
    
    Adds new rule: no-duplicate-id
    
    Update lib/rules/no-duplicate-id.js
    
    Co-Authored-By: Tobias Bieniek <tobias.bieniek@gmail.com>
    
    Update docs/rule/no-duplicate-id.md
    
    Add failing tests: Mustache + Concat
    
    Convert data structure of `already seen` ids: Array -> Set
    
    Add node check function that supports `@id`, `Concat`, `Mustache`
    
    Add support for String-only Mustache + Concats
    
    Abstract Mustache + Concat evaluation to utility functions
    
    Squash / cleanup for adding BlockStatement + elementId support
    
    Restore config
    
    removing errant vscode settings file
    
    Update lib/rules/no-duplicate-id.js
    
    Add failing tests for Mustache with NumberLiteral
    
    Add single-element passing tests for POC: Number, String, Mustache
    
    Add support for MustacheStatement with hashArg id attribute
    
    Add preliminary updates for NumberLiteral support
    
    Abstract logging to `logNode(msg, node)`
    
    Chore: remove dev/POC helpers + rename Set
    
    Revert logic for NumberLiteral + Chore: naming
    
    Add support for Mustache with dynamic value
    
    Add Mustache logic: isStringLiteral ? value : source
    
    Update node type-checking logic
    
    Import Mustache return helper for this context
    
    Partial refactor to `match`
    
    Refactor `isConcatString` via `match`
    
    Add reusable logNode + add support for Mustache
    
    Add scoping + support for unwrapping Concat parts
    
    POC: testNode/refNode testNode/refNodesArray
    
    Add checks to helper functions: existence
    
    Chore: naming convention
    
    Chore: naming convention
    
    Update tests: add parser + make examples relevant
    
    Add no-match test for [refNodes]
    
    POC: generic `match(testNode, refNode OR [refNode])`
    
    Consolidate to generic `.match()` function + update tests
    
    Remove package-lock.json
    
    Update docs/rule/no-duplicate-id.md
    
    Squash / cleanup for adding BlockStatement + elementId support
    
    Restore config
    
    removing errant vscode settings file
    
    Add failing tests for Mustache with NumberLiteral
    
    Add single-element passing tests for POC: Number, String, Mustache
    
    Add support for MustacheStatement with hashArg id attribute
    
    Add preliminary updates for NumberLiteral support
    
    Abstract logging to `logNode(msg, node)`
    
    Chore: remove dev/POC helpers + rename Set
    
    Revert logic for NumberLiteral + Chore: naming
    
    Add support for Mustache with dynamic value
    
    Add Mustache logic: isStringLiteral ? value : source
    
    Add Mustache logic: isStringLiteral ? value : source
    
    Update node type-checking logic
    
    Import Mustache return helper for this context
    
    Partial refactor to `match`
    
    Refactor `isConcatString` via `match`
    
    Add reusable logNode + add support for Mustache
    
    Add scoping + support for unwrapping Concat parts

--- test/unit/rules/no-duplicate-id-test.js
+++ test/unit/rules/no-duplicate-id-test.js
@@ -13,13 +13,15 @@ generateRuleTests({
     // Unique sibling TextNode IDs
     '<div id="id-00"></div><div id="id-01"></div>',
 
-    // Ignore MustacheStatements, even duplicates
-    '<div id={{this.divId}}></div>',
-    '<div id={{this.divId}}></div><div id={{this.divId}}></div>',
+    // MustacheStatements
+    '<div id={{"id-00"}}></div>',
+    '<div id={{"id-00"}}></div><div id={{"id-01"}}></div>',
+    '<div id={{this.divId00}}></div>',
+    '<div id={{this.divId00}}></div><div id={{this.divId01}}></div>',
 
-    // Ignore ConcatStatements, even duplicates
-    '<div id="concat-{{this.divId}}"></div>',
-    '<div id="concat-{{this.divId}}"></div><div id="concat-{{this.divId}}"></div>',
+    // ConcatStatements
+    '<div id="concat-{{this.divId00}}"></div>',
+    '<div id="concat-{{this.divId00}}"></div><div id="concat-{{this.divId01}}"></div>',
 
     // Mustache and Concat do not conflict/flag with TextNode
     '<div id={{id-00}}></div><div id="id-00"></div>',
@@ -27,9 +29,23 @@ generateRuleTests({
     '<div id="concat-{{id-00}}"></div><div id="concat-id-00"></div>',
     '<div id="concat-id-00"></div><div id="concat-{{id-00}}"></div>',
 
-    // BlockStatement
+    // SourceNode: BlockStatement
     '<div id="id-00"></div>{{#foo elementId="id-01"}}{{/foo}}',
     '{{#foo elementId="id-01"}}{{/foo}}<div id="id-00"></div>',
+
+    // [WIP] Number
+    '<div id={{1234}}></div>',
+    '<div id={{1234}}></div><div id={{"1234"}}></div>',
+
+    // [WIP] Dynamic
+    '<div id={{"id-00"}}></div><div id={{"id-01"}}></div>',
+    '<div id={{this.foo}}></div><div id={{this.bar}}></div>',
+
+    // Source: Mustache
+    '{{foo id="id-00"}}{{foo id="id-01"}}',
+
+    // [WIP] Mixed
+    '<div id="partA{{partB}}{{"partC"}}"></div><div id="{{"partA"}}{{"partB"}}partC"></div>',
   ],
 
   bad: [
@@ -160,6 +176,51 @@ generateRuleTests({
         line: 1,
         column: 34,
         source: '{{#bar elementId="id-00"}}{{/bar}}',
+      },
+    },
+
+    // Source Node: MustacheStatement
+    {
+      template: '{{foo id="id-00"}}{{foo id="id-00"}}',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 18,
+        source: '{{foo id="id-00"}}',
+      },
+    },
+
+    // MustacheStatement with NumberLiteral
+    {
+      template: '<div id={{1234}}></div><div id={{1234}}></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 28,
+        source: 'id={{1234}}',
+      },
+    },
+
+    // MustacheStatement with Dynamic/Computed Value
+    {
+      template: '<div id={{this.divId00}}></div><div id={{this.divId00}}></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 36,
+        source: 'id={{this.divId00}}',
+      },
+    },
+
+    // Mixed
+    {
+      template:
+        '<div id="partA{{partB}}{{"partC"}}"></div><div id="{{"partA"}}{{partB}}partC"></div>',
+      result: {
+        message: ERROR_MESSAGE,
+        line: 1,
+        column: 47,
+        source: 'id="{{"partA"}}{{partB}}partC"',
       },
     },
   ],
